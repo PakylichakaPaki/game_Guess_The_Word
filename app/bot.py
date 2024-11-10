62d017630529e92a844f5d6d9e037d484fd52acc
PakylichakaPaki <bershadskiyegor@mail.ru>
2024-11-11 02:07:51 +0700

    first

--- app/bot.py
+++ app/bot.py
@@ -1,26 +1,25 @@
 import random
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters.command import Command
 
 # Токен вашего бота
 API_TOKEN = "7955201672:AAFQK31GYD4cLahmk91iSD_0htMzCepEIA0"
 
-# Список слов для угадывания
+# Список слов для угадывания на русском 100 слов
+
 WORDS = [
-    "hello",
-    "world",
-    "python",
-    "aiogram",
-    "bot",
-    "game",
-    "hangman",
-    "word",
-    "guess",
-    "play",
-    "fun",
-]
+    "привет", "море", "яблоко", "собака", "кот", "дом", "машина", "книга", "город",
+    "дерево", "животное", "время", "день", "ночь", "зима", "лето", "весна", "осень", "река",
+    "гора", "небо", "облако", "цветок", "трава", "лес", "птица", "рыба", "звезда", "луна",
+    "солнце", "дождь", "снег", "ветер", "гроза", "радуга", "месяц", "год", "час",
+    "минута", "секунда", "деньги", "карта", "телефон", "компьютер", "интернет", "школа", "университет", "работа",
+    "отдых", "путешествие", "гостиница", "ресторан", "магазин", "рынок", "банк", "аптека", "полиция", "больница",
+    "парк", "сад", "озеро", "пруд", "река", "море", "океан", "пляж", "порт", "аэропорт",
+    "вокзал", "автобус", "поезд", "самолет", "машина", "велосипед", "мотоцикл", "лодка", "корабль", "яхта",
+    "спорт", "футбол", "хоккей", "баскетбол", "теннис", "плавание", "бег", "йога", "пилатес", "бодибилдинг",
+    "здоровье", "питание", "тренировка", "отдых", "сон"
+    ]
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
@@ -35,11 +34,17 @@ games = {}
 async def start_game(message: types.Message):
     # Выбираем случайное слово
     word = random.choice(WORDS)
+    if len(word) > 10:
+        valueword = 10
+    elif len(word) <= 10 and len(word) >= 5:
+        valueword = 7
+    else:
+        valueword = 5
     # Создаем состояние игры
     games[message.chat.id] = {
         "word": word,
         "guessed_letters": set(),
-        "attempts": 6,
+        "attempts": valueword,
     }
     # Отправляем сообщение с приглашением начать игру
     await message.answer(
@@ -84,17 +89,13 @@ async def guess_letter(message: types.Message):
 
     # Проверяем, выиграл ли игрок
     if word_state == games[chat_id]["word"]:
-        await message.answer(
-            f"Поздравляем! Вы угадали слово '{games[chat_id]['word']}'!"
-        )
+        await message.answer(f"Поздравляем! Вы угадали слово '{games[chat_id]['word']}'! Напиши команду /start чтобы начать новую игру.")
         del games[chat_id]
         return
 
     # Проверяем, проиграл ли игрок
     if games[chat_id]["attempts"] == 0:
-        await message.answer(
-            f"К сожалению, вы проиграли. Загаданное слово было '{games[chat_id]['word']}'."
-        )
+        await message.answer(f"К сожалению, вы проиграли. Загаданное слово было '{games[chat_id]['word']}'. Напиши команду /start чтобы начать новую игру.")
         del games[chat_id]
         return
 
@@ -103,7 +104,6 @@ async def guess_letter(message: types.Message):
 
 async def main():
     await dp.start_polling(bot)
-
-
+    
 if __name__ == "__main__":
     asyncio.run(main())
